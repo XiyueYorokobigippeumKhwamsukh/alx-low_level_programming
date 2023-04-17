88f806c83ca8daefeda2be4cf4d30207ce38ad8f
XiyueYorokobigippeumKhwamsukh <joeyceeetie@gmail.com>
2023-04-17 11:37:20 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef dog_h
+#define dog_h
+
+
+/**
+  * struct dog - new type
+  * @name: first member
+  * @age: second member
+  * @owner: third member
+  * Description: Longer decription
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
