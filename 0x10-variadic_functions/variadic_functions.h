165013eca531d2446dcc9859e3a8d87e7d61d011
XiyueYorokobigippeumKhwamsukh <joeyceeetie@gmail.com>
2023-04-20 13:59:40 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,5 @@
-#ifndef variadic_functions_h
-#define variadic_functions_
+#ifndef variadic_function_h
+#define variadic_functions_h
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
